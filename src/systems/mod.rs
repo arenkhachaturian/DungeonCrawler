fcf5d09607062248961e03bc1b0b8337c41f0f6c
Aren Khachatoorian <khachatoorianaren@gmail.com>
2025-01-17 03:43:42 +0400

    Implemented basic combat logic

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,31 +1,31 @@
 mod player_input;
 mod map_render;
 mod entity_render;
-mod collisions;
 mod random_move;
 mod end_turn;
 mod movement;
 mod hud;
 mod tooltips;
+mod combat;
 
 use crate::prelude::*;
 
 pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
-        .add_system(player_input::player_input_system())
-        .flush()
-        .add_system(map_render::map_render_system())
-        .add_system(entity_render::entity_render_system())
-        .add_system(hud::hud_system())
-        .add_system(tooltips::tooltips_system())
-        .build()
+    .add_system(player_input::player_input_system())
+    .flush()
+    .add_system(map_render::map_render_system())
+    .add_system(entity_render::entity_render_system())
+    .add_system(hud::hud_system())
+    .add_system(tooltips::tooltips_system())
+    .build()
 }
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
-        .add_system(movement::movement_system())
+        .add_system(combat::combat_system())
         .flush()
-        .add_system(collisions::collisions_system())
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
@@ -40,7 +40,7 @@ pub fn build_monster_scheduler() -> Schedule {
         .flush()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(collisions::collisions_system())
+        .add_system(combat::combat_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -18,11 +20,64 @@ pub fn player_input(
             VirtualKeyCode::Down => Point::new(0, 1),
             _ => Point::new(0, 0),
         };
-        players.iter(ecs).for_each(| (entity, pos) | {
-            let destination = *pos + delta;
-            commands
-            .push(((), WantsToMove{ entity: *entity, destination }));
-            });
-            *turn_state = TurnState::PlayerTurn;
+
+        let (player_entity, destination) = players
+            .iter(ecs)
+            .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
+            .unwrap();
+
+        let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+        let mut did_something = false;
+        if delta.x != 0 || delta.y != 0 {
+            let mut hit_something: bool = false;
+            did_something = true;
+            enemies
+                .iter(ecs)
+                .filter(|(_, pos)| **pos == destination)
+                .for_each(|(entity, _)| {
+                    hit_something = true;
+                    did_something = true;
+                    commands.push((
+                        (),
+                        WantsToAttack {
+                            attacker: player_entity,
+                            victim: *entity,
+                        },
+                    ));
+                });
+            if !hit_something {
+                did_something = true;
+                commands.push((
+                    (),
+                    WantsToMove {
+                        entity: player_entity,
+                        destination,
+                    },
+                ));
+            }
+        }
+        players.iter(ecs).for_each(|(entity, pos)| {
+            let destination: Point = *pos + delta;
+            commands.push((
+                (),
+                WantsToMove {
+                    entity: *entity,
+                    destination,
+                },
+            ));
+        });
+        if !did_something {
+            if let Ok(mut entry) = ecs.entry_mut(player_entity) {
+                if let Ok(health) = entry.get_component_mut::<Health>() {
+                    health.current = i32::min(health.max, health.current + 1);
+                } else {
+                    println!("Failed to get Health component.");
+                }
+            } else {
+                println!("Failed to get entry for player_entity.");
+            }
+            
+        }
+        *turn_state = TurnState::PlayerTurn;
     }
 }
